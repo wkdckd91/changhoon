5022f3ae7328720afadb986ca99d610c0804e94b
wkdckd91 <wkdckd41@gmail.com>
2025-02-25 18:18:36 +0900

    feat: 할 일 수정 , 삭제 API 구현

---
+++ src/main/java/org/example/web/vo/ResultResponse.java
@@ -0,0 +1,13 @@
+package org.example.web.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class ResultResponse {
+    private boolean isSuccess;
+
+}
